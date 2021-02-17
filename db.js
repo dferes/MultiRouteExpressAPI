914daec5295952ceb0529aea847a43d0c529d138
dferes <dferes23@gmail.com>
2021-02-17 15:51:02 -0800

    had test db assinged to non testing environment

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Client } = requre ('pg');
 let DB_URI;
 
-DB_URI = process.env.NODE_ENV === 'test' ? 'postgresql:///biztime': 'postgresql:///biztime_test';
+DB_URI = process.env.NODE_ENV === 'test' ? 'postgresql:///biztime_test' : 'postgresql:///biztime';
 
 let db = new Client({
     connectionstring: DB_URI
