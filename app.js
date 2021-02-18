9f538f853931c45061c7436095a5903de581830a
dferes <dferes23@gmail.com>
2021-02-17 20:39:11 -0800

    added invoice routes

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const ExpressError = require("./expressError")
 app.use(express.json());
 
 const companyRoutes = require('./routes/companies');
-// const invoiceRoutes = require('./routes/invoices');
+const invoiceRoutes = require('./routes/invoices');
 
 app.use('/companies', companyRoutes);
+app.use('/invoices', invoiceRoutes);
 
 
 /* 404 handler */
