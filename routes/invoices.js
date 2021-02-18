4a1d5a21b2be4b8e258ea9e69b2dff45c7d28ea7
dferes <dferes23@gmail.com>
2021-02-17 21:57:25 -0800

    all routes have been added for invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -55,7 +55,11 @@ router.post('/', async (req, res, next) => {
         if (!comp_code || !amt) {
             throw new ExpressError('Missing JSON data, must provide an invoice comp_code and amount', 400);
         }
-        const results = await db.query('INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, paid_date', [comp_code, amt]);
+        const results = await db.query(
+            `INSERT INTO invoices (comp_code, amt) 
+             VALUES ($1, $2) 
+             RETURNING id, comp_code, amt, paid, add_date, paid_date`, 
+             [comp_code, amt]);
         return res.status(201).json({ invoice: results.rows[0] });
     } catch(e) {
        return next(e)
@@ -70,7 +74,12 @@ router.put('/:id', async (req, res, next) => {
             throw new ExpressError('Missing JSON data, must provide an invoice amount', 400);
         }
         
-        const result = await db.query('UPDATE invoices SET amt=$2 WHERE id=$1 RETURNING id, comp_code, amt, paid, add_date, paid_date', [req.params.id, amt]);
+        const result = await db.query(
+            `UPDATE invoices 
+             SET amt=$2 
+             WHERE id=$1 
+             RETURNING id, comp_code, amt, paid, add_date, paid_date`, 
+             [req.params.id, amt]);
         
         if(result.rows.length === 0) {
             throw new ExpressError(`No invoice with an id ${req.params.id} exists`, 404);
