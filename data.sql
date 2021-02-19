ece37f594c6fe54a70859de786383fc8b7965e57
dferes <dferes23@gmail.com>
2021-02-18 18:12:22 -0800

    added a few tables and inserts

--- data.sql
+++ data.sql
@@ -1,7 +1,9 @@
-\c biztime
+\c biztime_test
 
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industries_companies;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,6 +21,17 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+    code text PRIMARY KEY,
+    industry text NOT NULL UNIQUE
+);
+
+CREATE TABLE industries_companies (
+    id serial PRIMARY KEY,
+    industry_code text NOT NULL REFERENCES industries ON DELETE CASCADE,
+    company_code text NOT NULL REFERENCES companies ON DELETE CASCADE
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -28,3 +41,14 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries (code, industry)
+  VALUES ('comp', 'Computers'),
+         ('petr', 'Petroleum'),
+         ('man', 'Manufacturing');
+
+INSERT INTO industries_companies (industry_code, company_code)
+  VALUES ('comp', 'apple'),
+         ('comp', 'ibm'),
+         ('man', 'apple'),
+         ('man', 'ibm');
